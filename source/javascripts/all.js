1954f941e4ecccf218dc7a436f7a153ac1602d7b
annanee <annaflavia@fluture.com.br>
2016-05-16 23:49:05 -0300

    New intro animation structure

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -20,40 +20,52 @@ $(function()
               $('#navbarScroll').removeClass('navbar-scrolled');
               $('#navbarScroll .reprograma-logo .light-purple').removeClass('light-purple').addClass('white');
           }
+      });
+
+      var loadAnimation = function() {
+          var animateHash = function() {
+              $leftKey = $('#leftKey');
+              $rightKey = $('#rightKey');
+              $leftKey.animate({"left": "-=300px", "opacity":1}, "slow");
+              $rightKey.animate({"right": "-=300px", "opacity":1}, "slow");
+          };
+
+          window.onload = animateHash;
+
+          setTimeout(function() {
+              $leftKey.animate({"left": "0", "opacity":1}, "slow");
+              $rightKey.animate({"left": "-=300px", "opacity":1}, "slow");
+              removeAnimationFrame();
+          }, 4500);
       }
-      );
 
-      var animateHash = function() {
-          $leftKey = $('#leftKey');
-          $rightKey = $('#rightKey');
-          $leftKey.animate({"left": "-=300px", "opacity":1}, "slow");
-          $rightKey.animate({"right": "-=300px", "opacity":1}, "slow");
-      };
+      var loadTypingAnimation = function() {
+          var theater = theaterJS();
 
-      window.onload = animateHash;
+          theater
+              .on('type:start, erase:start', function () {
+                  var actor = theater.getCurrentActor()
+                  actor.$element.classList.add('is-typing')
+              })
+              .on('type:end, erase:end', function () {
+                  var actor = theater.getCurrentActor()
+                  actor.$element.classList.remove('is-typing')
+              })
 
-      setTimeout(function() {
-          $leftKey.animate({"left": "0", "opacity":1}, "slow");
-          $rightKey.animate({"left": "-=300px", "opacity":1}, "slow");
-      }, 4500);
+          theater
+              .addActor('typed')
 
-      var theater = theaterJS();
+          theater
+              .addScene('typed:reprograma', 2000)
+              .addScene(-10)
+      }
 
-      theater
-          .on('type:start, erase:start', function () {
-              var actor = theater.getCurrentActor()
-              actor.$element.classList.add('is-typing')
-          })
-          .on('type:end, erase:end', function () {
-              var actor = theater.getCurrentActor()
-              actor.$element.classList.remove('is-typing')
-          })
+      var removeAnimationFrame = function() {
+          setTimeout(function() {
+              $('#intro').fadeOut('slow');
+          }, 1800);
+      }
 
-      theater
-          .addActor('typed')
-
-      theater
-          .addScene('typed:reprograma', 2000)
-          .addScene(-10)
-  }
-);
+      loadAnimation();
+      loadTypingAnimation();
+  });
