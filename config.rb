04cecf696c1a87714274225f2b19df364901f3be
annanee <annaflavia@fluture.com.br>
2016-05-17 23:38:51 -0300

    Configured GA reprograma tag

--- config.rb
+++ config.rb
@@ -1,38 +1,11 @@
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
-
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-# General configuration
 
 # Reload the browser automatically whenever files change
 configure :development do
   activate :livereload
 end
-
-###
-# Helpers
-###
-
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
 
 # Build-specific configuration
 configure :build do
@@ -49,4 +22,9 @@ end
 activate :deploy do |deploy|
   deploy.deploy_method = :git
   deploy.build_before = true
+end
+
+# Google Analytics Reprogrma
+activate :google_analytics do |ga|
+  ga.tracking_id = 'UA-77865189-1'
 end
